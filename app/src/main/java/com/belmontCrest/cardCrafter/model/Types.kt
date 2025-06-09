cea6c35aaf8ae974e7604f951664e24a3c09766c
Xanderlmk <Alex.ar911.ar@gmail.com>
2025-06-08 19:50:21 -0700

    Remove anything unrelated to the Webpage.

--- app/src/main/java/com/belmontCrest/cardCrafter/model/Types.kt
+++
@@ -1,9 +0,0 @@
-package com.belmontCrest.cardCrafter.model
-
-object Type {
-    const val BASIC = "basic"
-    const val HINT = "hint"
-    const val THREE = "three"
-    const val MULTI = "multi"
-    const val NOTATION = "notation"
-}
